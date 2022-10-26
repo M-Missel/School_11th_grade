f151b1050c45a4a201dccebc18ff991ad3175662
Axtinator <m.missel@protonmail.com>
2022-10-26 15:01:11 +0200

    school assignment

---
+++ CSharp/Bank_Calc_Capital/Bank_Calc_Capital/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Bank_Calc_Capital
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double dBankValue = 1000;
+            int iyears = 0;
+            /*
+            for (int i = 0; i <= 12; i++)
+            {
+                dBankValue = dBankValue + dBankValue * 0.05;
+            }
+
+            Console.WriteLine("The Value of the Capital is after 12 years: {0} Euro", Math.Round(dBankValue, 2));
+            */
+
+            for (int i = 0; dBankValue <= 2000; i++)
+            {
+                dBankValue = dBankValue + dBankValue * 0.05;
+                iyears++;
+            }
+
+            Console.WriteLine("After {0} the Bankvalue Was doubled", iyears);
+        }
+    }
+}
---
+++ CSharp/Sum_Odd_Numbers/Sum_Odd_Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Sum_Odd_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int iOutput = 0;
+
+            Console.Write("Enter an odd Number: ");
+            int iOdd = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 1; i <= iOdd; i+=2)
+            {
+                iOutput = iOutput + i;
+            }
+
+            Console.WriteLine("The Sum of all odd numbers between 1 and {0} is: {1}", iOdd, iOutput);
+
+            Console.ReadKey();
+        }
+    }
+}
