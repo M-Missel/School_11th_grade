fe24432bf1bcddc86362945b1667619849fdcd29
Axtinator <m.missel@protonmail.com>
2022-11-21 20:15:19 +0100

    while_loop

---
+++ CSharp/while_loop_Euclide/while_loop_Euclide/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace while_loop_Euclide
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ia, ib, itemp;
+
+            Console.Write("Enter the first Number: ");
+            ia = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the second Number: ");
+            ib = Convert.ToInt32(Console.ReadLine());
+
+            while (ib != 0)
+            {
+                itemp = ib;
+                ib = (ia % ib);
+                ia = itemp;
+            }
+
+            Console.WriteLine(ia);
+        }
+    }
+}
