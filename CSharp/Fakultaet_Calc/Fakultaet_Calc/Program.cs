ff7e169dc6bb03239fcba2011eaa99a18722c0bb
Axtinator <m.missel@protonmail.com>
2022-11-23 17:09:13 +0100

    Geldwechselproblem (loops)

---
+++ CSharp/Fakultaet_Calc/Fakultaet_Calc/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Fakultaet_Calc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int iInput, iOutput = 1;
+
+            Console.Write("Geben Sie die Fakultaet ein: ");
+            iInput = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= iInput; i++)
+            {
+                iOutput = iOutput * i;
+            }
+
+            Console.WriteLine("Faluktaet von {0} ist {1}",iInput, iOutput);
+        }
+    }
+}
---
+++ CSharp/Geldwechselproblem/Geldwechselproblem/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Geldwechselproblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int iInput, irest, i13Rubel;
+
+            Console.Write("Geldbetrag: ");
+            iInput = Convert.ToInt32(Console.ReadLine());
+
+            irest = iInput % 13;
+            i13Rubel = iInput / 13;
+
+            for (int i = 1; irest % 8 != 0; i++)
+            {
+                i13Rubel--;
+                irest += 13;
+            }
+
+            Console.WriteLine("{0} = 8 Rubel {1} = 13 Rubel", irest / 8, i13Rubel);
+        }
+    }
+}
