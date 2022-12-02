7a2415bd7865048d7d2467ea84e43a5a8cbe9aac
Axtinator <m.missel@protonmail.com>
2022-12-02 19:17:55 +0100

    Tasks Pseudo-Code to Code

---
+++ CSharp/Guess_numbers/Guess_numbers/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+/*
+    Task: Convert pseudocode into code
+*/
+
+namespace Guess_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int iRandom, iCount = 0, iold_difference = 100, inew_difference, iInput = 0;
+            Random random = new Random();
+
+            iRandom = random.Next(1, 101);
+
+            Console.WriteLine("Guess my Number between 1 and 100\n\nThe distance to the number is first given as 100.");
+
+            Console.WriteLine(iRandom);
+
+            do
+            {
+                Console.Write("\t...\nPlease enter a Number: ");
+                iInput = Convert.ToInt32(Console.ReadLine());
+
+                inew_difference = Math.Abs(iRandom - iInput);
+
+                if (inew_difference < iold_difference)
+                {
+                    if (inew_difference > 0)
+                    {
+                        Console.WriteLine("\t... -> You have approached in this round");
+                    }
+                }
+                else
+                {
+                    if (inew_difference > iold_difference)
+                    {
+                        Console.WriteLine("\t... -> You have distanced yourself in this round");
+                    }
+                    else
+                    {
+                        Console.WriteLine("\t... -> Nothing changed");
+                    }
+                }
+                Console.WriteLine();
+                iCount++;
+                iold_difference = inew_difference;
+                
+            } while (inew_difference != 0);
+
+            Console.WriteLine("Congratulations\n You have guessed the number in {0} tries", iCount);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharp/divisor_of_a_number/divisor_of_a_number/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace divisor_of_a_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char sControl = 'j';
+            int iInput, iCount;
+
+            Console.WriteLine("Output the Divisor of a Number\n-------------------------------");
+
+            while (sControl == 0x4A || sControl == 0x6A)
+            {
+                iCount = 0;
+                Console.Write("Please enter a interger Number: ");
+                iInput = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = iInput; i != 0; i--)
+                {
+                    if (iInput % i == 0)
+                    {
+                        iCount++;
+                        Console.WriteLine("The {0} divisor of {1} is: {2}", iCount, iInput, i);
+                    }
+                }
+
+                if (iCount == 2)
+                {
+                    Console.WriteLine("The entered number is a prime number and divisible only by 1 and itself\n");
+                }
+
+                Console.Write("Should the program be run again? (j/n): ");
+
+                try
+                {
+                    sControl = Convert.ToChar(Console.ReadLine());
+                }
+                catch
+                {
+
+                }
+            }
+
+            Console.WriteLine("Thank you for the usage of this Program");
+        }
+    }
+}
