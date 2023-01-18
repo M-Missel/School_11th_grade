daef2f6d83f053803bd9b073c3557a778b7fb72f
Axtinator <m.missel@protonmail.com>
2023-01-18 21:47:03 +0100

    Array assignment

---
+++ CSharp/Windows_Forms/AIT_Array_StringGrid/AIT_Array_StringGrid/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace AIT_Array_StringGrid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SG_OUTPUT = new StringGrid.StringGrid();
+            this.BTN_INIT = new System.Windows.Forms.Button();
+            this.BTN_MAX_VALUE = new System.Windows.Forms.Button();
+            this.BTN_MIN_VALUE = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SG_OUTPUT
+            // 
+            this.SG_OUTPUT.ColCount = 10;
+            this.SG_OUTPUT.DefaultColWidth = 25;
+            this.SG_OUTPUT.DefaultRowHeight = 25;
+            this.SG_OUTPUT.FixedCols = 0;
+            this.SG_OUTPUT.FixedRows = 0;
+            this.SG_OUTPUT.GridLineWidth = 1;
+            this.SG_OUTPUT.Location = new System.Drawing.Point(266, 132);
+            this.SG_OUTPUT.Name = "SG_OUTPUT";
+            this.SG_OUTPUT.RowCount = 10;
+            this.SG_OUTPUT.Size = new System.Drawing.Size(262, 262);
+            this.SG_OUTPUT.TabIndex = 0;
+            // 
+            // BTN_INIT
+            // 
+            this.BTN_INIT.Location = new System.Drawing.Point(354, 25);
+            this.BTN_INIT.Name = "BTN_INIT";
+            this.BTN_INIT.Size = new System.Drawing.Size(75, 23);
+            this.BTN_INIT.TabIndex = 1;
+            this.BTN_INIT.Text = "Init";
+            this.BTN_INIT.UseVisualStyleBackColor = true;
+            this.BTN_INIT.Click += new System.EventHandler(this.BTN_INIT_ARRAY);
+            // 
+            // BTN_MAX_VALUE
+            // 
+            this.BTN_MAX_VALUE.Location = new System.Drawing.Point(354, 54);
+            this.BTN_MAX_VALUE.Name = "BTN_MAX_VALUE";
+            this.BTN_MAX_VALUE.Size = new System.Drawing.Size(75, 23);
+            this.BTN_MAX_VALUE.TabIndex = 2;
+            this.BTN_MAX_VALUE.Text = "Max Value";
+            this.BTN_MAX_VALUE.UseVisualStyleBackColor = true;
+            this.BTN_MAX_VALUE.Click += new System.EventHandler(this.BTN_SEARCH_MAX);
+            // 
+            // BTN_MIN_VALUE
+            // 
+            this.BTN_MIN_VALUE.Location = new System.Drawing.Point(354, 83);
+            this.BTN_MIN_VALUE.Name = "BTN_MIN_VALUE";
+            this.BTN_MIN_VALUE.Size = new System.Drawing.Size(75, 23);
+            this.BTN_MIN_VALUE.TabIndex = 3;
+            this.BTN_MIN_VALUE.Text = "Min Value";
+            this.BTN_MIN_VALUE.UseVisualStyleBackColor = true;
+            this.BTN_MIN_VALUE.Click += new System.EventHandler(this.BTN_SEARCH_MIN);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BTN_MIN_VALUE);
+            this.Controls.Add(this.BTN_MAX_VALUE);
+            this.Controls.Add(this.BTN_INIT);
+            this.Controls.Add(this.SG_OUTPUT);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private StringGrid.StringGrid SG_OUTPUT;
+        private System.Windows.Forms.Button BTN_INIT;
+        private System.Windows.Forms.Button BTN_MAX_VALUE;
+        private System.Windows.Forms.Button BTN_MIN_VALUE;
+    }
+}
+
---
+++ CSharp/Windows_Forms/AIT_Array_StringGrid/AIT_Array_StringGrid/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AIT_Array_StringGrid
+{
+    public partial class Form1 : Form
+    {
+        int[,] ints = new int[10,10];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void BTN_INIT_ARRAY(object sender, EventArgs e)
+        {
+            fillArray(ints);
+            showArray(ints);
+        }
+
+        public void BTN_SEARCH_MAX(object sender, EventArgs e)
+        {
+            searchMax(ints);
+        }
+
+        public void BTN_SEARCH_MIN(object sender, EventArgs e)
+        {
+            searchMin(ints);
+        }
+
+        private void searchMax(int[,] array)
+        {
+            int controll = array[0,0];
+            int[] xypos = { -1, -1 };
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int k = 0; k < array.GetLength(1); k++)
+                {
+                    if (array[i,k] > controll)
+                    {
+                        controll = array[i,k];
+                        xypos[0] = i;
+                        xypos[1] = k;
+                    }
+                }
+            }
+
+            Output_Message(xypos[0], xypos[1], controll);
+        }
+
+        private void searchMin(int[,] array)
+        {
+            int[] xypos = { -1, -1 };
+            int minValue = array[0, 0];
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int k = 0; k < array.GetLength(1); k++)
+                {
+                    if (minValue > array[i, k])
+                    {
+                        minValue = array[i, k];
+                        xypos[0] = i;
+                        xypos[1] = k;
+                    }
+                }
+            }
+
+            Output_Message(xypos[0], xypos[1], minValue);
+        }
+
+        private int[,] fillArray(int[,] array)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int k = 0; k < array.GetLength(1); k++)
+                {
+                    array[i, k] = random.Next(0,51);
+                }
+            }
+
+            return array;
+        }
+
+        private void showArray(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int k = 0; k < array.GetLength(1); k++)
+                {
+                    SG_OUTPUT[i, k] = array[i, k].ToString();
+                }
+            }
+        }
+
+        private void Output_Message(int xpos, int ypos, int value)
+        {
+            MessageBox.Show($"The Object with the searched Value is in position {xpos} | {ypos} and has the Value {value}");
+        }
+    }
+}
