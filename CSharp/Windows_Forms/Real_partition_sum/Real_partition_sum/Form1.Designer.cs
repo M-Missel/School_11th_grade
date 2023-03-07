98e75d3d58b3228cd76b18e4f04f53e4009ae59a
Axtinator <m.missel@protonmail.com>
2023-03-07 17:03:44 +0100

    Methods

---
+++ CSharp/Windows_Forms/Real_partition_sum/Real_partition_sum/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Real_partition_sum
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TB_Number = new System.Windows.Forms.TextBox();
+            this.TB_Output = new System.Windows.Forms.TextBox();
+            this.BTN_ETS = new System.Windows.Forms.Button();
+            this.LB_Output = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TB_Number
+            // 
+            this.TB_Number.Location = new System.Drawing.Point(310, 68);
+            this.TB_Number.Name = "TB_Number";
+            this.TB_Number.Size = new System.Drawing.Size(181, 20);
+            this.TB_Number.TabIndex = 0;
+            // 
+            // TB_Output
+            // 
+            this.TB_Output.Location = new System.Drawing.Point(309, 160);
+            this.TB_Output.Name = "TB_Output";
+            this.TB_Output.Size = new System.Drawing.Size(181, 20);
+            this.TB_Output.TabIndex = 1;
+            // 
+            // BTN_ETS
+            // 
+            this.BTN_ETS.Location = new System.Drawing.Point(310, 106);
+            this.BTN_ETS.Name = "BTN_ETS";
+            this.BTN_ETS.Size = new System.Drawing.Size(180, 32);
+            this.BTN_ETS.TabIndex = 2;
+            this.BTN_ETS.Text = "Calculation";
+            this.BTN_ETS.UseVisualStyleBackColor = true;
+            this.BTN_ETS.Click += new System.EventHandler(this.BTN_Calc_Ets);
+            // 
+            // LB_Output
+            // 
+            this.LB_Output.FormattingEnabled = true;
+            this.LB_Output.Location = new System.Drawing.Point(310, 275);
+            this.LB_Output.Name = "LB_Output";
+            this.LB_Output.Size = new System.Drawing.Size(179, 147);
+            this.LB_Output.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(310, 223);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 32);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Calculation_2";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.BTN_Calc_Ets_Numbers);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.LB_Output);
+            this.Controls.Add(this.BTN_ETS);
+            this.Controls.Add(this.TB_Output);
+            this.Controls.Add(this.TB_Number);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TB_Number;
+        private System.Windows.Forms.TextBox TB_Output;
+        private System.Windows.Forms.Button BTN_ETS;
+        private System.Windows.Forms.ListBox LB_Output;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CSharp/Windows_Forms/Real_partition_sum/Real_partition_sum/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Real_partition_sum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BTN_Calc_Ets(object sender, EventArgs e)
+        {
+            TB_Output.Text = Convert.ToString(calc_ets(Convert.ToInt32(TB_Number.Text)));
+        }
+
+        private void BTN_Calc_Ets_Numbers(object sender, EventArgs e)
+        {
+            for (int i = 2; i <= Convert.ToInt32(TB_Number.Text); i++)
+            {
+                LB_Output.Items.Add(calc_ets(i));
+            }
+        }
+
+        private int calc_ets(int number)
+        {
+            int output = 0;
+
+            for (int i = 1; i <= number/2; i++)
+            {
+                if (number % i == 0)
+                {
+                    output += i;
+                }
+            }
+
+            return output;
+        }
+    }
+}
