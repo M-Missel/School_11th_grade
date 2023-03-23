3342fafae00e61a6535bd327d9fd5b1ac554031c
Axtinator <m.missel@protonmail.com>
2023-03-23 17:29:57 +0100

    Five_in_a_row

---
+++ CSharp/Windows_Forms/Five_in_a_row/Five_in_a_row/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Five_in_a_row
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SG_Output = new StringGrid.StringGrid();
+            this.BTN_Start = new System.Windows.Forms.Button();
+            this.LB_Player = new System.Windows.Forms.Label();
+            this.LB_Player_Next = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SG_Output
+            // 
+            this.SG_Output.ColCount = 10;
+            this.SG_Output.DefaultColWidth = 25;
+            this.SG_Output.DefaultRowHeight = 25;
+            this.SG_Output.FixedCols = 1;
+            this.SG_Output.FixedRows = 1;
+            this.SG_Output.GridLineWidth = 1;
+            this.SG_Output.Location = new System.Drawing.Point(51, 26);
+            this.SG_Output.Name = "SG_Output";
+            this.SG_Output.RowCount = 8;
+            this.SG_Output.Size = new System.Drawing.Size(262, 210);
+            this.SG_Output.TabIndex = 0;
+            this.SG_Output.SelectCell += new StringGrid.StringGrid.SelectCellEventHandler(this.setStone);
+            // 
+            // BTN_Start
+            // 
+            this.BTN_Start.Location = new System.Drawing.Point(51, 251);
+            this.BTN_Start.Name = "BTN_Start";
+            this.BTN_Start.Size = new System.Drawing.Size(261, 29);
+            this.BTN_Start.TabIndex = 1;
+            this.BTN_Start.Text = "Start";
+            this.BTN_Start.UseVisualStyleBackColor = true;
+            this.BTN_Start.Click += new System.EventHandler(this.BTN_Start_Click);
+            // 
+            // LB_Player
+            // 
+            this.LB_Player.AutoSize = true;
+            this.LB_Player.Location = new System.Drawing.Point(59, 308);
+            this.LB_Player.Name = "LB_Player";
+            this.LB_Player.Size = new System.Drawing.Size(73, 13);
+            this.LB_Player.TabIndex = 2;
+            this.LB_Player.Text = "Current Player";
+            // 
+            // LB_Player_Next
+            // 
+            this.LB_Player_Next.AutoSize = true;
+            this.LB_Player_Next.Location = new System.Drawing.Point(159, 308);
+            this.LB_Player_Next.Name = "LB_Player_Next";
+            this.LB_Player_Next.Size = new System.Drawing.Size(0, 13);
+            this.LB_Player_Next.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 450);
+            this.Controls.Add(this.LB_Player_Next);
+            this.Controls.Add(this.LB_Player);
+            this.Controls.Add(this.BTN_Start);
+            this.Controls.Add(this.SG_Output);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private StringGrid.StringGrid SG_Output;
+        private System.Windows.Forms.Button BTN_Start;
+        private System.Windows.Forms.Label LB_Player;
+        private System.Windows.Forms.Label LB_Player_Next;
+    }
+}
+
---
+++ CSharp/Windows_Forms/Five_in_a_row/Five_in_a_row/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Five_in_a_row
+{
+    public partial class Form1 : Form
+    {
+        char[,] chars = new char[10, 8];
+        char currentPlayer = 'W';
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void BTN_Start_Click(object sender, EventArgs e)
+        {
+            startGame();
+            SG_Output.Enabled = true;
+        }
+
+        private void writeChar(int x_pos, int y_pos, char mark)
+        {
+            chars[x_pos, y_pos] = mark;
+        }
+
+        private char readChar(int x_pos, int y_pos)
+        {
+            return chars[x_pos, y_pos];
+        }
+
+        private void init_Array()
+        {
+            for (int i = 1; i < chars.GetLength(1); i++)
+            {
+                for (int k = 1; k < chars.GetLength(0); k++)
+                {
+                    chars[k, i] = 'F';
+                }
+            }
+        }
+
+        private void writeArrayToStringGrid()
+        {
+            for (int i = 1; i < chars.GetLength(1); i++)
+            {
+                for (int k = 1; k < chars.GetLength(0); k++)
+                {
+                    SG_Output[k,i] = (chars[k, i] != 'F') ? chars[k, i].ToString() : " ";
+                }
+            }
+        }
+
+        private void showPlayer()
+        {
+            LB_Player_Next.Text = $"The Player {currentPlayer} turn"; 
+        }
+
+        private void startGame()
+        {
+            init_Array();
+            writeArrayToStringGrid();
+            showPlayer();
+        }
+
+        public void setStone(object sender, StringGrid.SelectCellEventArgs e)
+        {
+            if (readChar(e.ACol, e.ARow) == 'F')
+            {
+                writeChar(e.ACol, e.ARow, currentPlayer);
+                writeArrayToStringGrid();
+                if (returnMaxValue(e.ACol, e.ARow) >= 5)
+                {
+                    MessageBox.Show($"The User {currentPlayer} wins the game");
+                    SG_Output.Enabled = false;
+                }
+                else
+                {
+                    changePlayer();
+                }
+            }
+        }
+
+        private void changePlayer()
+        {
+            currentPlayer = (currentPlayer == 'W') ? 'S' : 'W';
+        }
+
+        private int checkVertical(int x_pos)
+        {
+            int counter = 0;
+            for (int i = 1; i < chars.GetLength(1); i++)
+            {
+                if (chars[x_pos, i] == currentPlayer)
+                    counter++;
+                else
+                    counter = 0;
+            }
+
+            return counter;
+        }
+
+        private int checkHorizontal(int y_pos)
+        {
+            int counter = 0;
+            for (int i = 1; i < chars.GetLength(0); i++)
+            {
+                if (chars[i, y_pos] == currentPlayer)
+                    counter++;
+                else
+                    counter = 0;
+            }
+
+            return counter;
+        }
+
+        private int checkDiagonalLeft(int x_pos, int y_pos)
+        {
+            int temp_x_pos = x_pos -1;
+            int temp_y_pos = y_pos -1;
+            int counter = 0;
+
+            while (temp_x_pos >= 1 && temp_y_pos >= 1 && chars[temp_x_pos, temp_y_pos] == currentPlayer)
+            {
+                counter++;
+                temp_x_pos--;
+                temp_y_pos--;
+            }
+
+            while (x_pos <= chars.GetLength(0) -1 && y_pos <= chars.GetLength(1) -1 && chars[x_pos, y_pos] == currentPlayer)
+            {
+                counter++;
+                x_pos++;
+                y_pos++;
+            }
+
+            return counter;
+        }
+
+        private int checkDiagonalRight(int x_pos, int y_pos)
+        {
+            int temp_x_pos = x_pos - 1;
+            int temp_y_pos = y_pos - 1;
+            int counter = 0;
+
+            while (temp_x_pos >= 1 && temp_y_pos <= chars.GetLength(1) - 1 && chars[temp_x_pos, temp_y_pos] == currentPlayer)
+            {
+                counter++;
+                temp_x_pos--;
+                temp_y_pos++;
+            }
+
+            while (x_pos <= chars.GetLength(0) - 1 && y_pos >=1 && chars[x_pos, y_pos] == currentPlayer)
+            {
+                counter++;
+                x_pos++;
+                y_pos--;
+            }
+
+            return counter;
+        }
+
+        private int returnMaxValue(int x_pos, int y_pos)
+        {
+            return Math.Max(checkHorizontal(y_pos), Math.Max(checkVertical(x_pos), Math.Max(checkDiagonalLeft(x_pos, y_pos), checkDiagonalRight(x_pos, y_pos))));
+        }
+    }
+}
